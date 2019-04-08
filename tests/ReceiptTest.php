80b832682533ea3e8f49723d135814b01541e997
Triin Vaht <triinvaht@gmail.com>
2019-04-08 11:02:09 +0300

    Second Test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -6,15 +6,27 @@ use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 class ReceiptTest extends TestCase {
+    public function setUp()
+    {
+        //add function setUpand
+        //add this arrow Receipt is equal to new Receiptinside of the setUp method
+        $this->Receipt = new Receipt();
+    }
+    //we'll add public function tearDown
+    public function tearDown() {
+        // we'll add unset this arrow Receipt
+        unset($this->Receipt);
+    }
+    //we'll add variable output is equal to this arrow Receipt arrow total, pass in our variable input, add our semicolon and we'll take output and set it on line 21
     public function testTotal() {
-        //create an Object from Receipt Class
-        $Receipt = new Receipt();
-        //php Test Method
+        $input = [0,2,5,8];
+        $output = $this->Receipt->total($input);
+
         $this->assertEquals(
+        //we will erase the Receipt arrow total
         //expectec  value
             15,
-            //actual value
-            $Receipt->total([0,2,5,8]),
+            $output,
             'When summing the total should equal 15'
         );
     }
